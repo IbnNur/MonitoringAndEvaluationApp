cc6042cc696dab10939be62bebb6490ba544fa30
Nazir Daudo <39951469+IbnNur@users.noreply.github.com>
2022-06-21 16:39:56 +0200

    Monitoring And Evaluation App

---
+++ app/src/main/java/monitoring/and/evaluation/MainActivity.java
@@ -0,0 +1,24 @@
+package monitoring.and.evaluation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText username = (EditText) findViewById(R.id.username);
+        EditText password = (EditText) findViewById(R.id.password);
+        MaterialButton  loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
+        TextView forgotpass = (TextView) findViewById(R.id.forgotpass);
+    }
+}
