d047f861e8f2909f08805fb54aade86811ebca29
Nazir Daudo <39951469+IbnNur@users.noreply.github.com>
2022-06-23 08:16:12 +0200

    Monitoring And Evaluation App

---
+++ app/src/main/java/monitoring/and/evaluation/CadastroOperador.java
@@ -0,0 +1,14 @@
+package monitoring.and.evaluation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadastroOperador extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_operador);
+    }
+}
